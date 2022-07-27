d7e41019cf7b60164de708434c43af1f075a81b4
Diego Kreutz <diego.unipampa@gmail.com>
2022-07-27 09:27:47 -0300

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# permissoes
 chmod u+x ./building/run_building.sh
 chmod u+x ./labelling/run_n_labellings.sh
 chmod u+x ./labelling/virustotal/run.sh
@@ -8,3 +10,10 @@ chmod u+x ./download/run_n_downloads.sh
 chmod u+x ./extraction/run_apk_extraction.sh
 chmod u+x ./extraction/run_n_extractions.sh
 chmod u+x kill_all.sh
+
+# dependências
+sudo snap install curl
+sudo apt install time
+sudo python3 -m pip install androguard==3.3.5
+sudo python3 -m pip install networkx==2.2
+sudo python3 -m pip install pandas==1.3.5
